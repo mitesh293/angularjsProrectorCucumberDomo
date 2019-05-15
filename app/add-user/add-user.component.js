4f0747333ff99c7a1a5f9a26792c956168e53cc5
mitesh293 <pmitesh293@gmail.com>
2019-05-15 16:01:54 +0530

    swipecode

--- app/add-user/add-user.component.js
+++ app/add-user/add-user.component.js
@@ -12,6 +12,10 @@ component('addUser', {
         $scope.userList = [{'name':'Juliya','surname':'Wastson','email':'Joily.Wastan@gmail.com','gender':false},
                             {'name':'Michal','surname':'clark','email':'micky.clarl@hotmail.com','gender':true}];
 
+        $scope.secondGrid=[];
+        $scope.firstGrid;
+        $scope.firstGridData=[{id:'1',name:'Dealer'},{id:'2',name:'Model Year'},{id:'3',name:'Manufacturer'},{id:'4',name:'Collaterial'}];
+
     $scope.submit = function() {
         var JsonString = {'name':$scope.name,'surname':$scope.surname,'email':$scope.email,'gender':$scope.gender};
        // console.log(JsonString);
@@ -32,6 +36,24 @@ component('addUser', {
         angular.forEach($scope.userList, function(user){
             user.selected = $scope.selectedAll;
         });
+    }
+    $scope.shiftLeft = function(){
+        $scope.temp=[];
+        $scope.firstGridData.forEach(element => {
+            if($scope.firstGrid.includes(element.id)){
+                $scope.secondGrid.push(element);
+                $scope.temp.push(element);
+            }
+        });
+        $scope.temp.forEach(element =>{
+            $scope.firstGridData.splice($scope.firstGridData.indexOf(element),1);
+        });
+    }
+    $scope.shiftLeftAll = function(){
+        $scope.firstGridData.forEach(element => {
+                $scope.secondGrid.push(element);
+        });
+        $scope.firstGridData=[];
     }
 }
 ]);
